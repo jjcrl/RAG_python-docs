fb55ecbd595bb626d7ec7f4128ae924c0770b776
jjcrl <j412cff@icloud.com>
2026-04-14 21:36:23 +0100

    added more tests and more logic to pattern detecting

---
+++ lib/detect_patterns.py
@@ -0,0 +1,51 @@
+import os
+current_dir = os.getcwd()
+path = os.path.join(current_dir,'corpus')
+
+def detect_pattern(filepath):
+    if filepath.endswith('glossary.txt'):
+        return 'D'
+    else:
+        with open(filepath,"r",encoding="utf-8") as f:
+            lines= f.readlines()
+        has_equals = False
+        has_dashes = False
+        has_signatures = False
+
+        for line in lines:
+            is_signature = (
+                line and not 
+                line.startswith(" ") and 
+                "|" not in line and 
+                ('(' in line or line.startswith("@")) and 
+                (line.rstrip().endswith(")") or line.rstrip().endswith("):"))
+                )
+            
+            if is_signature:
+                has_signatures = True
+
+            stripped = line.strip()
+            if len(stripped) > 2 and all(char == "=" for char in stripped):
+                has_equals = True
+            elif len(stripped) > 3 and all(char == "-"for char in stripped):
+                has_dashes = True
+
+        if has_equals and has_dashes:
+            return 'C'
+        elif has_equals:
+            return "A"
+        elif has_signatures:
+            return "B"
+        else:
+            return "E"
+
+                
+
+
+for root,dirs,files in os.walk(path):
+    for file in files:
+        if file.endswith(".txt"):
+            filepath = os.path.join(root,file)
+            print(filepath,detect_pattern(filepath))
+
+
--- tests/test_detect_patterns.py
+++ tests/test_detect_patterns.py
@@ -1,4 +1,4 @@
-from src.detect_patterns import detect_pattern
+from lib.detect_patterns import detect_pattern
 
 def test_glossary():
     assert detect_pattern("corpus/glossary.txt") == "D"
@@ -8,3 +8,12 @@ def test_faq():
 
 def test_sorting():
     assert detect_pattern("corpus/howto/sorting.txt") == "A"
+
+def test_function():
+    assert detect_pattern("corpus/library/functions.txt") == "B"
+
+def test_functions():
+    assert detect_pattern("corpus/library/functions.txt") == "B"
+
+def test_appetite():
+    assert detect_pattern("corpus/tutorial/appetite.txt") == "E"
